c706f3d58404d363006646a513bfae4f245fefa7
Yohannes Terfa <lightshadow011@gmail.com>
2021-10-29 03:09:44 -0700

    task 1 done

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a list using
+ * insertion sort algorithm
+ * @list: the list to sort
+ * Return: (void)
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *head1, *head2, *next;
+	bool stop;
+
+	if (list)
+	{
+		head1 = *list;
+		head2 = *list;
+		while (list && head1->next)
+		{
+			if (head1->next)
+			{
+				stop = false;
+				head2 = head1;
+				while (head2 && head2->n > head2->next->n)
+				{
+					current = head2;
+					next = head2->next;
+					current->next = next->next;
+					if (next->next)
+						next->next->prev = current;
+					if (next)
+					{
+						next->prev = current->prev;
+						next->next = current;
+					}
+					if (current)
+						current->prev = next;
+					if (next->prev)
+						next->prev->next = next;
+					head2 = next->prev;
+					if (!next->prev)
+						*list = next;
+					print_list(*list);
+					stop = 1;
+				}
+			}
+			if (!flag)
+				head1 = head1->next;
+		}
+	}
+}
