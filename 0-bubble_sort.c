5936239721dd3b54d4af7f781f82c665f995854a
Yohannes Terfa <lightshadow011@gmail.com>
2021-10-29 02:26:58 -0700

    task 0 done

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using
+ * bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: (void)
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size - i; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
